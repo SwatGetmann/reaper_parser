51f4edbbceeb3671ae49b148c565da64e4c07802
SwatGetmann <swatgetmann@gmail.com>
2023-05-30 18:29:01 +0400

    dirty note block implementation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 
-from typing import AnyStr
+from typing import AnyStr, List
 
 class ReaperProject:
     """
@@ -19,26 +19,59 @@ class ReaperProject:
         open_block_r = r"\s*<([A-Z_]+)"
         end_block_r = r"^\s*\>\n"
         
+        active_block_name = None
+        
+        notes_block = Notes([])
+        
         for line_idx, line in enumerate(lines):
             if re.match(open_block_r, line):
                 match = re.search(open_block_r, line)
                 # print(line)
                 # print(match)
-                stack.append(match.group(1))
-                index_map[match.group(1)] = line_idx
+                active_block_name = match.group(1)
+                stack.append(active_block_name)
+                index_map[active_block_name] = line_idx
+                
+                if active_block_name == 'NOTES':
+                    pass
             elif re.search(end_block_r, line):
                 el_tag = stack.pop()
+                if stack:
+                    active_block_name = stack[-1]
                 print("<{}>, {} to {}".format(
                     el_tag, index_map[el_tag], line_idx
                 ))
-                
+            else:
+                # we're inside the block
+                if active_block_name == 'NOTES':
+                    notes_block.append_line(line)
+        
+        print(notes_block)
+           
+    
+class Notes:
+    """
+    Notes block from Reaper Project (*.rpp) files.
+    """
+    
+    def __init__(self, lines: List[AnyStr]) -> None:
+        if lines:
+            self.lines = lines
+        else:
+            self.lines = []
+    
+    def append_line(self, line: AnyStr) -> None:
+        self.lines.append(line)
+    
+    def __str__(self) -> str:
+        return "\n".join(self.lines)
                 
         
 
 if __name__ == '__main__':
     print("Welcome to Reaper Parser.")
     
-    # test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\221012 01 Tbilisi Sunrise\\221012 01 Zoom Rec Again.rpp"
-    test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\230503 three ideas\\230503 three ideas.rpp"
+    test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\221012 01 Tbilisi Sunrise\\221012 01 Zoom Rec Again.rpp"
+    # test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\230503 three ideas\\230503 three ideas.rpp"
         
     ReaperProject(test_reaper_project_fpath)
