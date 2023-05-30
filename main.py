2b880f1516a875eafa0d555270a4252f561e8d32
SwatGetmann <swatgetmann@gmail.com>
2023-05-30 17:39:23 +0400

    small dumb proof that xml does not work!

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from xml.dom.minidom import parse
 
 if __name__ == '__main__':
     print("Welcome to Reaper Parser.")
+    
+    test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\221012 01 Tbilisi Sunrise\\221012 01 Zoom Rec Again.rpp"
+    
+    dom1 = parse(test_reaper_project_fpath)
+    
+    print(dom1)
