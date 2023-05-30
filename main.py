464548d0868d4f945b8dd3d625bfedb3dfe87d4a
SwatGetmann <swatgetmann@gmail.com>
2023-05-30 18:11:00 +0400

    try another test file

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class ReaperProject:
 if __name__ == '__main__':
     print("Welcome to Reaper Parser.")
     
-    test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\221012 01 Tbilisi Sunrise\\221012 01 Zoom Rec Again.rpp"
+    # test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\221012 01 Tbilisi Sunrise\\221012 01 Zoom Rec Again.rpp"
+    test_reaper_project_fpath = "d:\\05 Music Lab\\Вдохновения\\Tbilisi Acoustic G\\230503 three ideas\\230503 three ideas.rpp"
         
     ReaperProject(test_reaper_project_fpath)
