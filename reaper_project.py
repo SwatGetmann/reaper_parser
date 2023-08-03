d0393207c3c8669121a69874afd178e9f89f8448
SwatGetmann <swatgetmann@gmail.com>
2023-08-03 18:16:06 +0400

    remoe trailing whitespaces and shitty method

--- reaper_project.py
+++ reaper_project.py
@@ -8,83 +8,83 @@ class ReaperProject:
     """
     Parser for *.rpp Reaper Project files.
     """
-   
+
     def __init__(self, filepath: str) -> None:
-       self.filepath = filepath
-       self.parse()
-    
+        self.filepath = filepath
+        self.parse()
+
     def parse(self) -> None:
         print("Processing file: ... {}".format(self.filepath))
-        
+
         with open(self.filepath) as f:  
             lines = f.readlines()
-        
+
         # !TODO: Index Map - what is it for? \
         # * What are the use cases?
         # * Are we planning to use it somehow?
         index_map = {}
-        
+
         stack = []
-        
+
         open_block_r = r"\s*<([A-Z_]+)"
         end_block_r = r"^\s*\>\n"
-        
+
         node_token = None
-                
+
         head = Node(ntype=NodeType.REAPER_PROJECT)
         prev = None
-        
+
         # TODO! : handle multiline parameters
         multiline_flag = False
         single_line_param_r = r"\s+([A-Z0-9_]+)\s+"
         uuid_line_param_r = r"\s+(\{{1}[A-F0-9-]+\}{1})\s+"
-        
+
         for line_idx, line in enumerate(lines):
             if re.match(open_block_r, line):
                 match = re.search(open_block_r, line)
-                
+
                 # print(line)
                 # print(match)
-                
+
                 node_token = match.group(1)
                 stack.append(node_token)
                 index_map[node_token] = line_idx
-                
+
                 inner_level = len(stack)
-                
+
                 # print(inner_level)
-                
+
                 if inner_level > 1:
                     prev = head
                     head = Node(ntype=NodeType[node_token])
                     head.prev = prev
                     head.depth = inner_level - 1
-                
+
                     # process line (header parameters) according to node.type
-                
+
             elif re.search(end_block_r, line):
                 el_tag = stack.pop()
                 inner_level -= 1
-                
+
                 # print("Closing block, inner level: {}".format(inner_level))
                 # print(inner_level)
-                
+
                 if param and multiline_flag:
                     head.parameters.append(param)
                     param = None
                     multiline_flag = False
-                
+
                 if inner_level > 0:
                     curr = head
                     head = head.prev
                     prev = head.prev
-                    
+
                     head.inner.append(curr)
-                    
+
                     node_token = stack[-1]
 
                 # print(head)
-                    
+
                 print("Level: {} :: <{}>, {} to {}".format(
                     inner_level+1, el_tag, index_map[el_tag], line_idx
                 ))
@@ -99,7 +99,7 @@ class ReaperProject:
                     # param = Parameter(type=ParameterType.TEXT) # for test
                     param.values.append({'ID': id})
                     param.values.append(line)
-                    
+
                     head.parameters.append(param)
                 else:
                     # multiline_flag = False
@@ -116,13 +116,5 @@ class ReaperProject:
                     param = Parameter(type=ParameterType.TEXT)
                 if param and multiline_flag:
                     param.values.append(line)
-                    
-        self.print_node_tree(head)
-    
-    def print_node_tree(self, head: Node) -> None:
-        """Prints node tree, from a given head
 
-        Args:
-            head (Node): a node to print (sub-)tree from
-        """
         head.print_tree()
