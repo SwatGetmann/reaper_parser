be725788cd858fb27ac8d177495f21c597a43e0d
SwatGetmann <swatgetmann@gmail.com>
2023-08-04 11:30:45 +0400

    put parsing loop right under the file reading

--- reaper_project.py
+++ reaper_project.py
@@ -33,14 +33,7 @@ class ReaperProject:
         """
         print(f"Processing file: ... {self.filepath}")
 
-        with open(self.filepath) as f:
-            lines = f.readlines()
-
-        # !TODO: Index Map - what is it for? \
-        # * What are the use cases?
-        # * Are we planning to use it somehow?
         index_map = {}
-
         stack = []
 
         node_token = None
@@ -51,78 +44,79 @@ class ReaperProject:
         # TODO! : handle multiline parameters
         multiline_flag = False
 
-        for line_idx, line in enumerate(lines):
-            if re.match(self.BLOCK_OPEN_RGX, line):
-                match = re.search(self.BLOCK_OPEN_RGX, line)
+        with open(self.filepath) as f:
+            for line_idx, line in enumerate(f):
+                if re.match(self.BLOCK_OPEN_RGX, line):
+                    match = re.search(self.BLOCK_OPEN_RGX, line)
 
-                # print(line)
-                # print(match)
+                    # print(line)
+                    # print(match)
 
-                node_token = match.group(1)
-                stack.append(node_token)
-                index_map[node_token] = line_idx
+                    node_token = match.group(1)
+                    stack.append(node_token)
+                    index_map[node_token] = line_idx
 
-                inner_level = len(stack)
+                    inner_level = len(stack)
 
-                # print(inner_level)
+                    # print(inner_level)
 
-                if inner_level > 1:
-                    prev = head
-                    head = Node(ntype=NodeType[node_token])
-                    head.prev = prev
-                    head.depth = inner_level - 1
+                    if inner_level > 1:
+                        prev = head
+                        head = Node(ntype=NodeType[node_token])
+                        head.prev = prev
+                        head.depth = inner_level - 1
 
-                    # process line (header parameters) according to node.type
+                        # process line (header parameters) according to node.type
 
-            elif re.search(self.BLOCK_END_RGX, line):
-                el_tag = stack.pop()
-                inner_level -= 1
+                elif re.search(self.BLOCK_END_RGX, line):
+                    el_tag = stack.pop()
+                    inner_level -= 1
 
-                # print("Closing block, inner level: {}".format(inner_level))
-                # print(inner_level)
+                    # print("Closing block, inner level: {}".format(inner_level))
+                    # print(inner_level)
 
-                if param and multiline_flag:
-                    head.parameters.append(param)
-                    param = None
-                    multiline_flag = False
+                    if param and multiline_flag:
+                        head.parameters.append(param)
+                        param = None
+                        multiline_flag = False
 
-                if inner_level > 0:
-                    curr = head
-                    head = head.prev
-                    prev = head.prev
+                    if inner_level > 0:
+                        curr = head
+                        head = head.prev
+                        prev = head.prev
 
-                    head.inner.append(curr)
+                        head.inner.append(curr)
 
-                    node_token = stack[-1]
+                        node_token = stack[-1]
 
-                # print(head)
+                    # print(head)
 
-                level_log_msg = f"L {inner_level+1} :: <{el_tag}>, {index_map[el_tag]} to {line_idx}"
-                print(level_log_msg)
-            elif not multiline_flag and re.search(self.PARAM_LINE_SINGLE_RGX, line):
-                if node_token == 'SWSAUTOCOLOR':
-                    get_param = single_line_create_param(
-                        line, 
-                        line_idx, 
-                        self.PARAM_LINE_UUID_RGX,
-                        ParameterType.SWSCOLOR_ID
-                    )
-                    param = get_param(lambda p, m: p.values.append({'ID': m}))
-                    head.parameters.append(param)
+                    level_log_msg = f"L {inner_level+1} :: <{el_tag}>, {index_map[el_tag]} to {line_idx}"
+                    print(level_log_msg)
+                elif not multiline_flag and re.search(self.PARAM_LINE_SINGLE_RGX, line):
+                    if node_token == 'SWSAUTOCOLOR':
+                        get_param = single_line_create_param(
+                            line, 
+                            line_idx, 
+                            self.PARAM_LINE_UUID_RGX,
+                            ParameterType.SWSCOLOR_ID
+                        )
+                        param = get_param(lambda p, m: p.values.append({'ID': m}))
+                        head.parameters.append(param)
+                    else:
+                        get_param = single_line_create_param(
+                            line, 
+                            line_idx, 
+                            self.PARAM_LINE_SINGLE_RGX
+                        )
+                        param = get_param()
+                        head.parameters.append(param)
                 else:
-                    get_param = single_line_create_param(
-                        line, 
-                        line_idx, 
-                        self.PARAM_LINE_SINGLE_RGX
-                    )
-                    param = get_param()
-                    head.parameters.append(param)
-            else:
-                if not multiline_flag:
-                    multiline_flag = True
-                    param = Parameter(type=ParameterType.TEXT)
-                if param and multiline_flag:
-                    param.values.append(line)
+                    if not multiline_flag:
+                        multiline_flag = True
+                        param = Parameter(type=ParameterType.TEXT)
+                    if param and multiline_flag:
+                        param.values.append(line)
 
         return head
 
