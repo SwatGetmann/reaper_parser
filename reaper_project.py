25b5854df6cc94b1a73458181493897caaf02c64
SwatGetmann <swatgetmann@gmail.com>
2023-08-07 13:06:50 +0400

    refactoring, moving out logic to methods

--- reaper_project.py
+++ reaper_project.py
@@ -1,5 +1,5 @@
 import re
-
+from typing import List
 from node import Node, NodeType
 from parameter import Parameter, ParameterType
 from parameter_decorators import single_line_create_param
@@ -62,50 +62,17 @@ class ReaperProject:
                         head.depth = inner_level - 1
 
                     # process line (header parameters) according to node.type
-                    print(line)
-                    line_split = line.split(f"<{node_token}")
-                    line_params_str = line_split[1].strip()
-                    
-                    line_str_stack = []
-                    param_strs = []
-                    l_pos = 0
-                    r_pos = 0
-                    for ci, c in enumerate(line_params_str):
-                        if c == '"':
-                            if len(line_str_stack) > 0:
-                                r_pos = ci
-                                line_str_stack.pop()
-                                # print(f"FOUND A SS: {ci} :: {c} :: L {l_pos} , R {r_pos} ;; {line_params_str[l_pos:r_pos]}")
-                                param_strs.append(line_params_str[l_pos:r_pos])
-                                l_pos = ci+1
-                                r_pos = ci+1
-                            else:
-                                l_pos = ci+1
-                                r_pos = ci+1
-                                line_str_stack.append(c)
-                        elif c == " ":
-                            if len(line_str_stack) > 0:
-                                r_pos += 1
-                            elif len(line_str_stack) == 0:
-                                if l_pos < r_pos:
-                                    # print(f"FOUND A WORD :: L {l_pos} , R {r_pos} ;; {line_params_str[l_pos:r_pos]}")
-                                    param_strs.append(line_params_str[l_pos:r_pos])
-                                l_pos = ci+1
-                                r_pos = ci+1
-                        else:
-                            r_pos += 1
-                        # print(f"{ci} :: {c} :: L {l_pos} , R {r_pos} ;; {line_params_str[l_pos:r_pos]}")
-                    # dirty hack, but leave for now
-                    if len(line_params_str) > 0 and len(param_strs) == 0:
-                        param_strs.append(line_params_str)
-                    print(param_strs)
+                    param_strs = line_param_parsing(line, node_token)
                     head.parameters_first_line = param_strs
 
                 elif re.search(self.BLOCK_END_RGX, line):
-                    el_tag = stack.pop()
+                    closed_node_token = stack.pop()
                     inner_level -= 1
 
                     if param and multiline_flag:
+                        # Stop collecting multiline params
+                        # TODO : join lines ?
+                        # TODO (?) : change aabstraction to first line, NEXT LINES, parse based by type WHEN NEEDED, lazy style
                         head.parameters.append(param)
                         param = None
                         multiline_flag = False
@@ -114,14 +81,13 @@ class ReaperProject:
                         curr = head
                         head = head.prev
                         prev = head.prev
-
                         head.inner.append(curr)
-
                         node_token = stack[-1]
 
-                    level_log_msg = f"L {inner_level+1} :: <{el_tag}>, {index_map[el_tag]} to {line_idx}"
+                    level_log_msg = f"L {inner_level+1} :: <{closed_node_token}>, {index_map[closed_node_token]} to {line_idx}"
                     print(level_log_msg)
                 elif not multiline_flag and re.search(self.PARAM_LINE_SINGLE_RGX, line):
+                    # TODO: actually, we can use #line_param_parsing on sinlge lines to get param values instead of archiving the full string
                     if node_token == 'SWSAUTOCOLOR':
                         get_param = single_line_create_param(
                             line,
@@ -147,3 +113,44 @@ class ReaperProject:
                         param.values.append(line.strip())
 
         return head
+
+
+def line_param_parsing(line: str, node_token: str) -> List[str]:
+    print(line)
+    line_split = line.split(f"<{node_token}")
+    line_params_str = line_split[1].strip()
+
+    line_str_stack = []
+    param_strs = []
+    l_pos = 0
+    r_pos = 0
+    for ci, c in enumerate(line_params_str):
+        if c == '"':
+            if len(line_str_stack) > 0:
+                r_pos = ci
+                line_str_stack.pop()
+                # print(f"FOUND A SS: {ci} :: {c} :: L {l_pos} , R {r_pos} ;; {line_params_str[l_pos:r_pos]}")
+                param_strs.append(line_params_str[l_pos:r_pos])
+                l_pos = ci+1
+                r_pos = ci+1
+            else:
+                l_pos = ci+1
+                r_pos = ci+1
+                line_str_stack.append(c)
+        elif c == " ":
+            if len(line_str_stack) > 0:
+                r_pos += 1
+            elif len(line_str_stack) == 0:
+                if l_pos < r_pos:
+                    # print(f"FOUND A WORD :: L {l_pos} , R {r_pos} ;; {line_params_str[l_pos:r_pos]}")
+                    param_strs.append(line_params_str[l_pos:r_pos])
+                l_pos = ci+1
+                r_pos = ci+1
+        else:
+            r_pos += 1
+        # print(f"{ci} :: {c} :: L {l_pos} , R {r_pos} ;; {line_params_str[l_pos:r_pos]}")
+    # dirty hack, but leave for now
+    if len(line_params_str) > 0 and len(param_strs) == 0:
+        param_strs.append(line_params_str)
+    print(param_strs)
+    return param_strs
