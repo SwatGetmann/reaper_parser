66d642c31d8fe433c6abbd7e0abb13dfc80bd2e3
SwatGetmann <swatgetmann@gmail.com>
2023-08-10 14:33:59 +0400

    add first liine parameter support, saving lines

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,11 @@
+from typing import List
+
 class Node:
     """Class represents a Node from a Reaper Project File tree."""
     def __init__(self, name: str) -> None:
         self.nodes = []
         self.inner = []
+        self.first_line = None
         self.prev = None
         self.depth = 0
         self.name = name
@@ -20,9 +23,21 @@ class Node:
         node.prev = self
         node.depth = self.depth + 1
         self.nodes.append(node)
+        
+    def fetch_nodes(self, name: str, collection: list = []) -> List['Node']:
+        if self.name == name:
+            collection.append(self)
+        if self.nodes:
+            for i in self.nodes:
+                i.fetch_nodes(name, collection)
+        return collection
     
     def save_str_tree(self) -> str:
-        lines = [f"{' ' * self.depth * 2}<{self.name}"]
+        lines = []
+        if self.first_line:
+            lines.append(f"{' ' * self.depth * 2}<{self.first_line}")
+        else:
+            lines.append(f"{' ' * self.depth * 2}<{self.name}")
         for i in self.inner:
             lines += [f"{' ' * (self.depth+1) * 2}{i}"]
         for n in self.nodes:
@@ -30,12 +45,15 @@ class Node:
         lines.append(f"{' ' * self.depth * 2}>")
         return lines
 
-def tokenize_node(stream):
+
+def tokenize_node(stream) -> List[Node]:
     line_type = None
     node_curr = None
     
     lens = [len(l) for l in stream]
     max_line_len = max(lens)
+    
+    heads = []
     
     def context_msg():
         if node_curr:
@@ -46,12 +64,18 @@ def tokenize_node(stream):
 
     for lidx, line in enumerate(stream):
         if line.lstrip()[0] == '<':
-            node_name = line.lstrip()[1:].split(" ")[0].rstrip()
+            line_type = 'TAG_OPEN'
+            strip_line = line.lstrip()[1:]
+            splits = strip_line.split(" ")
+            node_name = splits[0].rstrip()
             node = Node(node_name)
+            if len(splits) > 1:
+                node.first_line = strip_line.rstrip()
             if node_curr:
                 node_curr.add_node(node)
             node_curr = node
-            line_type = 'TAG_OPEN'
+            if node_curr.depth == 0:
+                heads.append(node_curr)
         elif line.lstrip()[0] == '>':
             line_type = 'TAG_CLOSE'
             node_curr = node_curr.prev
@@ -59,3 +83,5 @@ def tokenize_node(stream):
             line_type = 'TAG_INNER'
             node_curr.inner.append(line.strip())
         print(f"[{lidx:4}]: {line.rstrip().ljust(max_line_len)} | {context_msg()}")
+    
+    return heads
