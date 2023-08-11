15886aeb267ca3843707533a22d6d582dad4ee4b
SwatGetmann <swatgetmann@gmail.com>
2023-08-11 14:38:41 +0400

    use tokenizer instead

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import datetime
-import sys
 import argparse
 import os
 
-from reaper_project import ReaperProject
-from node_type import NodeType
+from tokenizer import tokenize_node
 
 parser = argparse.ArgumentParser(
     description="Reaper Parser utility - parses any given *.rpp file (Reaper DAW)."
@@ -25,14 +22,14 @@ if __name__ == '__main__':
     print(args)
 
     if args.file_path:
-        rp = ReaperProject(args.file_path, debug_log=args.debug)
-        # rp.head.print_tree()
-        fetch_res = rp.head.fetch(NodeType.VST, [])
-        for res in fetch_res:
-            res.print_tree()
+        rps = tokenize_node(open(args.file_path, 'r').readlines(), supress_output=args.debug)
+        rps[0].print_tree()
+        # fetch_res = rp.head.fetch(NodeType.VST, [])
+        # for res in fetch_res:
+        #     res.print_tree()
         
-        fetch_res = rp.head.fetch(NodeType.NOTES, [])
-        for res in fetch_res:
-            res.print_tree()
+        # fetch_res = rp.head.fetch(NodeType.NOTES, [])
+        # for res in fetch_res:
+        #     res.print_tree()
     else:
         print("No file path had been given.")
